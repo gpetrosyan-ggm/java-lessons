94abf0c4ab37d63a20164f86ec610e23fcf87617
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2022-01-31 13:39:25 +0400

    added homeworks related string and arrays

---
+++ java-core-lessons/src/com/demo/lesson3/homework/ArraysHomeWork.java
@@ -0,0 +1,71 @@
+package com.demo.lesson3.homework;
+
+public class ArraysHomeWork {
+
+    public static void main(String[] args) {
+        oneDimensionalArraySimpleDemo();
+        oneDimensionalArrayDemo(5, 4);
+        oneDimensionalArrayDemo(3, 0);
+
+        multiDimensionalArraySimpleDemo();
+        multiDimensionalArrayDemo(4, 5, 1, 1);
+        multiDimensionalArrayDemo(3, 3, 2, 2);
+
+        // TODO uncomment it and fill with your values
+        // oneDimensionalArrayDemo();
+        // oneDimensionalArrayDemo();
+        // multiDimensionalArrayDemo();
+        // multiDimensionalArrayDemo();
+    }
+
+    /**
+     * 1. Declare One-Dimensional Array (int or double).
+     * 2. Fill it with values.
+     * 3. Print the array.
+     * <p>
+     * 1. Declare One-Dimensional Array with initialization.
+     * 2. Print the array
+     */
+    private static void oneDimensionalArraySimpleDemo() {
+
+    }
+
+    /**
+     * 1. Declare One-Dimensional Array (int or double) using @param size as size of array.
+     * 2. Fill it with values.
+     * 3. Print element with @param index.
+     *
+     * @param size  the size of new array
+     * @param index the index of element which need to print
+     */
+    private static void oneDimensionalArrayDemo(int size, int index) {
+
+    }
+
+    /**
+     * 1. Declare Multi-Dimensional Array (int or double).
+     * 2. Fill it with values.
+     * 3. Print the array.
+     * <p>
+     * 1. Declare Multi-Dimensional Array with initialization.
+     * 2. Print the array
+     */
+    private static void multiDimensionalArraySimpleDemo() {
+
+    }
+
+    /**
+     * 1. Declare Two-Dimensional Array (int or double) using @param size as size of array.
+     * 2. Fill it with values.
+     * 3. Print element with @param iIndex, jIndex.
+     *
+     * @param iSize  the size of parent array
+     * @param jSize  the size of sub array
+     * @param iIndex the index of parent element which need to print
+     * @param jIndex the index of sub element which need to print
+     */
+    private static void multiDimensionalArrayDemo(int iSize, int jSize, int iIndex, int jIndex) {
+
+    }
+
+}
---
+++ java-core-lessons/src/com/demo/lesson3/homework/StringsDemo.java
@@ -0,0 +1,44 @@
+package com.demo.lesson3.homework;
+
+public class StringsDemo {
+
+    public static void main(String[] args) {
+        stringSimpleDemo();
+        stringPrintDemo("Hello World!");
+        stringConcatDemo("Hi", 10, 13.3, 'A', true);
+
+        // TODO uncomment it and fill with your values
+        // stringPrintDemo();
+        // stringConcatDemo();
+    }
+
+    /**
+     * Declare new string and print it.
+     */
+    private static void stringSimpleDemo() {
+
+    }
+
+    /**
+     * Print the input @param str value.
+     *
+     * @param str the input string value
+     */
+    private static void stringPrintDemo(String str) {
+
+    }
+
+    /**
+     * Concat all input values and print it.
+     *
+     * @param str the input string value
+     * @param i   the input int value
+     * @param d   the input double value
+     * @param c   the input char value
+     * @param b   the input boolean value
+     */
+    private static void stringConcatDemo(String str, int i, double d, char c, boolean b) {
+
+    }
+
+}
