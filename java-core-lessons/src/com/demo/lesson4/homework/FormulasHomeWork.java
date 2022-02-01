861c2bfbd4b0e8697eb62ea3c1d35af34c4117a6
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2022-02-01 10:31:01 +0400

    added some of formulas for hw

---
+++ java-core-lessons/src/com/demo/lesson4/homework/FormulasHomeWork.java
@@ -0,0 +1,92 @@
+package com.demo.lesson4.homework;
+
+public class FormulasHomeWork {
+
+    public static void main(String[] args) {
+        squareSum(3, 5);
+        squareOfDifference(7, 9);
+        differenceOfSquares(6, 4);
+        differenceOfCubes(7, 2);
+        sumOfCubes(5, 3);
+        sumCube(8, 9);
+        differenceCube(4, 6);
+    }
+
+    /**
+     * Квадрат суммы
+     * (a + b)2 = a2 + 2ab + b2
+     *
+     * @param a the first int value
+     * @param b the second int value
+     */
+    private static void squareSum(int a, int b) {
+
+    }
+
+    /**
+     * Квадрат разности
+     * (a - b)2 = a2 - 2ab + b2
+     *
+     * @param a the first int value
+     * @param b the second int value
+     */
+    private static void squareOfDifference(int a, int b) {
+
+    }
+
+    /**
+     * Разность квадратов
+     * a2 - b2 = (a + b)(a - b)
+     *
+     * @param a the first int value
+     * @param b the second int value
+     */
+    private static void differenceOfSquares(int a, int b) {
+
+    }
+
+    /**
+     * Разность кубов
+     * a3 - b3 = (a - b)·(a2 + ab + b2)
+     *
+     * @param a the first int value
+     * @param b the second int value
+     */
+    private static void differenceOfCubes(int a, int b) {
+
+    }
+
+    /**
+     * Сумма кубов
+     * a3 + b3 = (a - b)(a2 -ab + b2)
+     *
+     * @param a the first int value
+     * @param b the second int value
+     */
+    private static void sumOfCubes(int a, int b) {
+
+    }
+
+    /**
+     * Куб суммы
+     * (a + b)3 = a3 + b3 + 3ab(a + b)
+     *
+     * @param a the first int value
+     * @param b the second int value
+     */
+    private static void sumCube(int a, int b) {
+
+    }
+
+    /**
+     * Куб разности
+     * (a - b)3 = a3 - b3 - 3ab(a - b)
+     *
+     * @param a the first int value
+     * @param b the second int value
+     */
+    private static void differenceCube(int a, int b) {
+
+    }
+
+}
