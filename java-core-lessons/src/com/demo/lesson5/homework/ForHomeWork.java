fecaa0fdd113355e96cc251aa1d4007089c0cc41
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2022-02-14 20:43:14 +0400

    init for loop

---
+++ java-core-lessons/src/com/demo/lesson5/homework/ForHomeWork.java
@@ -0,0 +1,89 @@
+package com.demo.lesson5.homework;
+
+public class ForHomeWork {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Write simple for example (գրել պարզ for օրինակ)
+     */
+    private static void simpleForDemo() {
+    }
+
+    /**
+     * Write simple while to for example (գրել պարզ while֊ից for կոնվետեր օրինակ)
+     */
+    private static void simpleWhileToForConvertDemo() {
+//        int i = 0;
+//        while (i < 10) {
+//            System.out.println(i);
+//            i++;
+//        }
+//        System.out.println("finish");
+
+    }
+
+    /**
+     * Print Odd numbers (տպել կենտ թվերը)
+     *
+     * @param num input value
+     */
+    private static void printOddNumbers(int num) {
+
+    }
+
+    /**
+     * Print even numbers (տպել զույգ թվերը)
+     *
+     * @param num input value
+     */
+    private static void printEvenNumbers(int num) {
+    }
+
+    /**
+     * Print even and odd numbers (տպել զույգ և կենտ թվերը)
+     * step = 1 (i++) (քայլը = 1)
+     * %
+     * 55 % 2 = 1
+     * 6 % 2 = 0
+     * example (օրինակ)
+     * 1 ֊ odd
+     * 2 ֊ even
+     * 3 ֊ odd
+     * 4 ֊ even
+     *
+     * @param num input value
+     */
+    private static void printEvenAndOddNumbersIf(int num) {
+    }
+
+    /**
+     * Print simple array (տպել պարզ array 2 ձևով)
+     * for( ; ; )
+     * foreach
+     */
+    private static void printArrayDemo(int[] arr) {
+
+    }
+
+    /**
+     * Print simple array (տպել պարզ array 2 ձևով)
+     * for( ; ; )
+     * foreach
+     */
+    private static void printArrayDemo(int[][] arr) {
+
+    }
+
+    /**
+     * Print simple array (տպել պարզ array 2 ձևով)
+     * for( ; ; )
+     * foreach
+     */
+    private static void printArrayDemo(int[][][] arr) {
+
+    }
+
+}
