7b44301dc45ea5c823a6d4207dfd2140a1f06e59
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2022-02-26 11:55:34 +0400

    Exercises 1.1 done

---
+++ java-core-lessons/src/com/demo/progresstests/exercies1/Exercise1_1.java
@@ -0,0 +1,29 @@
+package com.demo.progresstests.exercies1;
+
+/*
+Перед вами примеры объявления переменных.
+Укажите, какие из них правильные, а какие - ошибочные.
+Обоснуйте каждый ответ.
+
+    int 1x;
+
+    double t;
+    int t;
+
+    int f, f;
+
+    int x, X;
+    douЫe а; a1;
+
+    String kniga1 ; kniga2;
+
+    char znak1,znak2,znak_1;
+
+    int х, а, Ь;
+    douЫe y, z, x;
+
+    int x;
+    double X;
+ */
+public class Exercise1_1 {
+}
