0c6a39c714c924fa3e96f639e64dd04d009f2c57
Arnold <arnoldmartirosyan87@gmail.com>
2022-02-28 00:50:07 +0400

    Exercise2-11

---
+++ java-core-lessons/src/com/demo/progresstests/exercies2/Exercies2_11.java
@@ -0,0 +1,19 @@
+package com.demo.progresstests.exercies2;
+/*
+2.11
+Напишите класс, который принимает с клавиатуры два целых числа и
+выводит на экран следующие значения (каждое в отдельной строке):
+сумму введенных чисел, их произведение, разницу между первым и вторым,
+разницу между вторым и первым.
+ */
+
+public class Exercies2_11 {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 7;
+        System.out.println(" a + b = " + (a + b));
+        System.out.println(" a * b = " + (a * b));
+        System.out.println(" a - b = " + (a - b));
+        System.out.println(" b - a = " + (b - a));
+    }
+}
