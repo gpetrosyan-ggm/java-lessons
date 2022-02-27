3e934393f31a3c89a1e18272672b2c3a6f9d99d2
Arnold <arnoldmartirosyan87@gmail.com>
2022-02-28 00:31:42 +0400

    Exercise2-8

---
+++ java-core-lessons/src/com/demo/progresstests/exercies2/Exercies2_8.java
@@ -0,0 +1,12 @@
+package com.demo.progresstests.exercies2;
+/*
+Напишите класс, который принимает с клавиатуры целое число
+и затем выводит на экран его квадрат и его же куб, а между ними знак &.
+ */
+
+public class Exercies2_8 {
+    public static void main(String[] args) {
+        int a = 12;
+        System.out.println((a * a) + " & " + (a * a * a));
+    }
+}
