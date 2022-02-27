9fdf452cde0d964c2df93fb11a29e401daebc333
Arnold <arnoldmartirosyan87@gmail.com>
2022-02-27 22:03:36 +0400

    Exercise2-6

---
+++ java-core-lessons/src/com/demo/progresstests/exercies2/Exercies2_6.java
@@ -0,0 +1,21 @@
+package com.demo.progresstests.exercies2;
+/*
+Напишите класс, который принимает с клавиатуры целое число и выводит на экран число,
+которое больше введенного с клавиатуры на 10.
+ */
+
+public class Exercies2_6 {
+    public static void main(String[] args) {
+        // miguce xndir@ sxal em haskacel, mtacum eyi menak 1-in tarberak@ grem heriqa, dra hamar navsyaki 2-rdn el greci
+        //1-in tarberak
+        //int x = 7;
+        //System.out.println(" x = " + (x +10));
+
+        // 2-rd tarberak
+        exerciesDemo(5);
+    }
+    private static void exerciesDemo(int x){
+        System.out.println(" x = " + (x + 10));
+
+    }
+}
