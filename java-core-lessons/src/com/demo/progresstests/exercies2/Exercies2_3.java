cf1c417593dd44e85204be677c0560f93546c578
Arnold <arnoldmartirosyan87@gmail.com>
2022-02-27 21:41:39 +0400

    Exercise2_3

---
+++ java-core-lessons/src/com/demo/progresstests/exercies2/Exercies2_3.java
@@ -0,0 +1,15 @@
+package com.demo.progresstests.exercies2;
+/*
+Напишите класс, который присваивает переменной х значение 6,
+а затем выводит на экран: в первой строке - это значение,
+во второй - квадрат этого значения, в третьей - куб этого значения.
+ */
+
+public class Exercies2_3 {
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println("1-in " + x);
+        System.out.println("2-rd " + (x * x));
+        System.out.println("3-rd " + (x * x * x));
+    }
+}
