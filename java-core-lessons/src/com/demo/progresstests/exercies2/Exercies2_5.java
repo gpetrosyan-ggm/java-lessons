b888a42c23a751758d333412cf9f4d5bce817168
Arnold <arnoldmartirosyan87@gmail.com>
2022-02-27 21:51:04 +0400

    Exercise2_5

---
+++ java-core-lessons/src/com/demo/progresstests/exercies2/Exercies2_5.java
@@ -0,0 +1,16 @@
+package com.demo.progresstests.exercies2;
+/*
+Напишите класс, который присваивает двум переменным значения,
+а затем выводит на экран: в первой строке значения обеих переменных (с поясняющими надписями),
+во второй - произведение этих переменных (с поясняющей надписью),
+в третьей - сумму этих переменных (с поясняющей надписью).
+ */
+
+public class Exercies2_5 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println("a = " + a + "; b = " + b);
+        System.out.println(" ayd tveri gumar@ klini a + b = " + (a + b));
+    }
+}
