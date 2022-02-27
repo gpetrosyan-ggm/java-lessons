2f545d5b1ee0f273d275fb8ab96eabec087a38c4
Arnold <arnoldmartirosyan87@gmail.com>
2022-02-28 00:42:13 +0400

    Exercise2-10

---
+++ java-core-lessons/src/com/demo/progresstests/exercies2/Exercies2_10.java
@@ -0,0 +1,15 @@
+package com.demo.progresstests.exercies2;
+/*
+2.10
+Напишите класс, который принимает с клавиатуры целое число
+и выводит на экран следующие значения (каждое в отдельной строке): снова введенное число
+и число, противоположное введенному числу.
+ */
+
+public class Exercies2_10 {
+    public static void main(String[] args) {
+        int a = 17;
+        System.out.println(+a);
+        System.out.println(+(-a));
+    }
+}
