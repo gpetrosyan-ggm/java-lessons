734f6934314df08808cf41489d0f5eaa51813d27
Arnold <arnoldmartirosyan87@gmail.com>
2022-02-28 00:27:57 +0400

    Exercise2-7

---
+++ java-core-lessons/src/com/demo/progresstests/exercies2/Exercies2_7.java
@@ -0,0 +1,15 @@
+package com.demo.progresstests.exercies2;
+/*
+Напишите класс, который принимает с клавиатуры целое число и выводит на экран два новых числа
+(каждое в отдельное строке): число, больше введенного с клавиатуры на 6,
+и число, меньше введенного с клавиатуры на 12.
+ */
+
+public class Exercies2_7 {
+    public static void main(String[] args) {
+        int a = 23;
+        System.out.println("1. " + (a + 6));
+        System.out.println("2. " + (a - 12));
+
+    }
+}
