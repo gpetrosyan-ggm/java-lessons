f06e14a817cb4e659bea77f35edf62d162d8a1f9
Arnold <arnoldmartirosyan87@gmail.com>
2022-02-27 19:07:18 +0400

    Exercise1_1 tnayin@ arvac e

--- java-core-lessons/src/com/demo/progresstests/exercies1/Exercise1_1.java
+++ java-core-lessons/src/com/demo/progresstests/exercies1/Exercise1_1.java
@@ -26,4 +26,43 @@ package com.demo.progresstests.exercies1;
     double X;
  */
 public class Exercise1_1 {
+  /*  public static void main(String[] args) {
+
+        int 1x; // sxal e, popoxakan@ chi karox lini 1x, karar liner int x; kam int x1;
+
+        double t; // chisht e;
+        int t; // chisht e;
+
+        int f, f; // sxal e, nuyn popoxak@ veragrvac e 2 angam,
+
+        int x, X; // sxal chi bayc vonc sovorecrel e senseys petq e veragrver arandzin arandzin
+        // int x;
+        // int X;
+        double a; a1; // sxal e, popoxakaner@ mievnun tipin veragrveluc petq e arandznacvein storaketov (,)
+        // ayl voch te ket storaket (;), baci ayd lav kliner grver
+        // double a;
+        // double a1;
+
+
+        String kniga1 ; kniga2; // sxal e nuyn verevis berac orinakov
+
+        char znak1,znak2,znak_1; // chisht e bayc senseyi orinakov nax@ntreli e'
+        // char znak1;
+        // char znak2;
+        // char znak_1;
+
+        int х, а, Ь;
+        // int x;
+        // int a;
+        // int b;
+        double y, z, x; // sxal chi bayc ...
+       // double y;
+       // double z;
+       // double x;
+
+        int x;  // chisht e
+        double X; // chisht e
+    }
+
+   */
 }
