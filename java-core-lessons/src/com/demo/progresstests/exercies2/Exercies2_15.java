18d8028c5e8c8ada9663c08a7fc757c084a6edc0
Arnold <arnoldmartirosyan87@gmail.com>
2022-02-28 19:38:05 +0400

    Exercies2_15

---
+++ java-core-lessons/src/com/demo/progresstests/exercies2/Exercies2_15.java
@@ -0,0 +1,18 @@
+package com.demo.progresstests.exercies2;
+/*
+Напишите класс, который принимает с клавиатуры целое число,
+представляющее собой массу тела в граммах, и выводящую на экран массу
+этого тела в килограммах.
+ */
+
+import java.util.Scanner;
+
+public class Exercies2_15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" dzer qash@ gramnerov");
+
+        double ves = scanner.nextDouble();
+        System.out.println(" dzer qash@ kilogramnerov klini " + (ves / 1000) + " kg");
+    }
+}
