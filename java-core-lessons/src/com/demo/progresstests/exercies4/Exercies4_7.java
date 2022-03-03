a98eb0f69426404d0bc71395ba44abce3b4d03f9
Arnold <arnoldmartirosyan87@gmail.com>
2022-03-03 19:43:52 +0400

    glux 6 u 4.7 anteri e, ete 6.5 chisht em haskacel

--- java-core-lessons/src/com/demo/progresstests/exercies4/Exercies4_7.java
+++ java-core-lessons/src/com/demo/progresstests/exercies4/Exercies4_7.java
@@ -18,30 +18,20 @@ public class Exercies4_7 {
         int a100 = a / 100 % 10;
         int a10 = a / 10 % 10;
         int a1 = a % 10;
-        int i = 0;
-        int i1000 = 0;
-        int i100 = 0;
-        int i10 = 0;
-        int i1 = 0;
-        if (i == 0) {
-            if (a1000 % 2 == 0) {
-                System.out.println("arajin tiv@ zuyg e " + a1000);
-                 i1000 = 1;
-            } else ;
-            if (a100 % 2 == 0) {
-                System.out.println("erkrord tiv@ zuyg e " + a100);
-                 i100 = 1;
-            } else ;
-            if (a10 % 2 == 0) {
-                System.out.println("errord tiv@ zuyg e " + a10);
-                i10 = 1;
-            } else ;
-            if (a1 % 2 == 0) {
-                System.out.println("chorord tiv@ zuyg e " + a1);
-                 i1 = 1;
-            }
-            System.out.println(" zuyg tvanshanneri qanak@ klini " + (i1000 + i100 + i10 + i1));
-            scanner.close();
+        if (a1000 % 2 == 0) {
+            System.out.println("arajin tiv@ zuyg e " + a1000);
+        } else ;
+        if (a100 % 2 == 0) {
+            System.out.println("erkrord tiv@ zuyg e " + a100);
+        } else ;
+        if (a10 % 2 == 0) {
+            System.out.println("errord tiv@ zuyg e " + a10);
+        } else ;
+        if (a1 % 2 == 0) {
+            System.out.println("chorord tiv@ zuyg e " + a1);
         }
+        System.out.println(" zuyg tvanshanneri qanak@ klini " + (4 - a1000 % 2 - a100 % 2 - a10 % 2 - a1 % 2));
+        scanner.close();
     }
 }
+
---
+++ java-core-lessons/src/com/demo/progresstests/exercies6/Exercies6_5.java
@@ -0,0 +1,34 @@
+package com.demo.progresstests.exercies6;
+/*
+Напишите класс, который принимает с клавиатуры два целых числа и,
+если первое больше второго, выводит на экран их сумму, если же наоборот - выводит на экран
+их произведение. В случае же, если числа одинаковы, программа выводит на экран
+сообщение Числа равны.
+ */
+
+//выводит на экран их произведение
+// a - b te b - a te esim inch
+
+import java.util.Scanner;
+
+public class Exercies6_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("amboxjakan tiv ");
+        int a = scanner.nextInt();
+        System.out.println("eli amboxjakan tiv ");
+
+        int b = scanner.nextInt();
+        if (a > b){
+            System.out.println( a + b);
+        }
+        if (a == b){
+            System.out.println("tver@ havasar en");
+        }
+        if (a < b){
+            System.out.println(a - b);
+        }
+        scanner.close();
+
+    }
+}
