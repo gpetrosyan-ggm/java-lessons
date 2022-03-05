0af7d18f121f748add0b4763eaf56a91fa4beaff
Arnold <arnoldmartirosyan87@gmail.com>
2022-03-06 00:16:47 +0400

    Exercies8.25_1 en vaxt sxal eyi arel, hima dzeci

--- java-core-lessons/src/com/demo/progresstests/exercies8/Exercies8_25_1.java
+++ java-core-lessons/src/com/demo/progresstests/exercies8/Exercies8_25_1.java
@@ -8,21 +8,21 @@ public class Exercies8_25_1 {
         System.out.println("eranish tiv");
 
         int a = scanner.nextInt();
-        int a100;
-        int a10;
-        int a1;
-        a100 = a / 100;
-        a10 = a / 10 % 10;
-        a1 = a % 10;
+
+        int a100 = a / 100;
+        int a10 = a / 10 % 10;
+        int a1 = a % 10;
+
         if (a % 2 == 0) {
-            int k1 = a100 % 2 == 0 ? (a100 + 1) : (a100 - 1);
-            int k2 = a10%2 ==0 ? (a10 + 1) : (a10-1);
-            int k3 = a1%2==0 ? (a1+1) : (a1-1);
+            a100 = a100 % 2 == 0 ? (a100 + 1) : (a100 - 1);
+            a10 = a10 % 2 == 0 ? (a10 + 1) : (a10 - 1);
+            a1 = a1 % 2 == 0 ? (a1 + 1) : (a1 - 1);
             System.out.println((a100 * 100 + a10 * 10 + a1));
-        }else {
-            int k1 = a100 % 2 == 1 ? (a100 + 1) : (a100 - 1);
-            int k2 = a10%2 ==1 ? (a10 + 1) : (a10-1);
-            int k3 = a1%2==1 ? (a1+1) : (a1-1);
+
+        } else {
+            a100 = a100 % 2 == 1 ? (a100 + 1) : (a100 - 1);
+            a10 = a10 % 2 == 1 ? (a10 + 1) : (a10 - 1);
+            a1 = a1 % 2 == 1 ? (a1 + 1) : (a1 - 1);
             System.out.println((a100 * 100 + a10 * 10 + a1));
         }
         scanner.close();
