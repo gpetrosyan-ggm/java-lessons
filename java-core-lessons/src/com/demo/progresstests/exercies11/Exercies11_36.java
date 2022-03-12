dbaa7a255cbbbf6240eab3057451147487d6f456
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2022-03-12 19:23:28 +0400

    11.36 fixed

--- java-core-lessons/src/com/demo/progresstests/exercies11/Exercies11_36.java
+++ java-core-lessons/src/com/demo/progresstests/exercies11/Exercies11_36.java
@@ -2,11 +2,9 @@ package com.demo.progresstests.exercies11;
 /*
 Напишите класс, который читает два целых положительных числа в переменных А и В и проверяет,
 действительно ли А меньше, чем В.
-Если да, - класс должен вывести на экран А последовательных чисел,начиная с числа В,
-если нет, - класс должен вывести на экран В последовательных чисел, начиная с числа А.
+Если да, - класс должен вывести на экран А последовательных чисел,начиная с числа В, A < B -> B - A
+если нет, - класс должен вывести на экран В последовательных чисел, начиная с числа А. A > B - > A - B
  */
-//stex ete asuma stugi ardyoq a<b; partadir petqa if-ov stugum anem te tenarniov karam grem?
-// if te tenari, misht xndrica kaxvac.
 
 import java.util.Scanner;
 
@@ -20,19 +18,20 @@ public class Exercies11_36 {
         System.out.println("tiv");
         int b = scanner.nextInt();
 
-       if (a<b){
-           for (int i = b; i>a; i--){
-               b--;
-               System.out.println(b);
+        int start;
+        int end;
+        if (a <= b) {
+            start = b;
+            end = a;
+        } else {
+            start = a;
+            end = b;
+        }
 
-           }
-       }else if (a>b){
-           for (int i = a; i>b; i--){
-               a--;
-               System.out.println(a);
-
-           }
-       }
+        for (int i = start; i >= end; i--) {
+            System.out.println(i);
+        }
 
     }
+
 }
