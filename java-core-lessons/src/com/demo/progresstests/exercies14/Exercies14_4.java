0f1715c5b1642add680c5240dc12199965b95ea2
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2022-03-16 11:43:57 +0400

    solved 14.4 exersises

--- java-core-lessons/src/com/demo/progresstests/exercies14/Exercies14_4.java
+++ java-core-lessons/src/com/demo/progresstests/exercies14/Exercies14_4.java
@@ -7,50 +7,38 @@ package com.demo.progresstests.exercies14;
 В случае если одно из нужных значений невозможно определить (почему?},
 следует вывести на экран соответствующее текстовое сообщение.
  */
-// paymanneric mek@ chi katarvi ete random@ voroshi bolor tver@ tpi drakan kam bolorn el bacasakan
-// orinak karanq muxlojutyun anenq u start@ kan end@ tanq 0;
 public class Exercies14_4 {
     public static void main(String[] args) {
         int start = -40;
         int and = 40;
-        int c;
-
-        int a = (int) (Math.random() * (and - start + 1) + start);
-        System.out.println("1 : " + a);
-        c = a;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
         for (int i = 2; i < 51; i++) {
+            int num = (int) (Math.random() * (and - start + 1) + start);
+            System.out.println(i + " : " + num);
 
-            int b = (int) (Math.random() * (and - start + 1) + start);
-            System.out.println(i + " : " + b);
-            if (b != 0) { // es xndir@ shat cuning areci minjev es lucum@ gta, RUN tvi u 4 hat 0 gta, vorosheci if mtcnem
-                if (b > c) {
-                    c = b;
-                } else if (b < a) {
-                    a = b;
-                }
+            if (num == 0) {
+                continue;
+            }
+
+            if (num > 0 && num > max) {
+                max = num;
+            } else if (num < 0 && num < min) {
+                min = num;
             }
         }
-        // if (c < 0) {
-        //     System.out.println("amena mec drakan tiv@ bacakayum e");
-        // } else {
-        //     System.out.println("amena mec drakan tiv@ " + c);
-        // }
-        // if (a > 0) {
-        //     System.out.println("amena poqr bacasakan tiv@ bacakayum e");
-        // } else {
-        //     System.out.println("amena poqr bacasakan tic@ " + a);
-        // }
-        // skzbum senc eyi grel, bayc heto erb sax glux@ greci prca, hat ar hat noric sax stugeci u xndir@ kardaluc heto
-        // mi hat poqr nuans gta,
-        // В случае если одно из нужных значений невозможно определить (почему?) следует вывести на экран соответствующее текстовое сообщение.
-        // chi asum konkret vor@ chi voroshvum, dra hamar el greci senc, if u else krjatelov
 
-        if (a > 0 && c < 0) {
-            System.out.println("paymanneric mek@ chi katarvum");
+        if (max == Integer.MIN_VALUE) {
+            System.out.println("Max chi gtel");
         } else {
-            System.out.println("amena poqr bacasakan tiv@ " + a);
-            System.out.println("amena mec drakan tiv@ " + c);
+            System.out.println("amena mec drakan tiv@ " + max);
+        }
+        if (min == Integer.MAX_VALUE) {
+            System.out.println("Min chi gtel");
+        } else {
+            System.out.println("amena poqr bacasakan tiv@ " + min);
         }
     }
+
 }
