43f57863491ffa015f3107e07f6b1cd15ee80ef9
Arnold <arnoldmartirosyan87@gmail.com>
2022-03-20 17:12:21 +0400

    todos

--- java-core-lessons/src/com/demo/progresstests/exercies15/Exercies15_36.java
+++ java-core-lessons/src/com/demo/progresstests/exercies15/Exercies15_36.java
@@ -1,4 +1,28 @@
 package com.demo.progresstests.exercies15;
+/*
+Напишите класс, который принимает с клавиатуры целое число и определяет,
+является ли оно точным квадратом целого числа.
+ */
+
+import java.util.Scanner;
 
 public class Exercies15_36 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("number");
+        int num = scanner.nextInt();
+
+        while (true){
+            if (num<=0){
+                break;
+            }
+            for (int i = 1; i < num;i++){
+                if (num==i*i){
+                    System.out.println(i+"-n handisanum e" );
+                }
+            }
+
+        }
+    }
 }
