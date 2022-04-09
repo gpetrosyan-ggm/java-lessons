4d715f5107af6db40aee02ccdddf76c7f91ab86b
Arnold <arnoldmartirosyan87@gmail.com>
2022-04-10 01:35:45 +0400

    Default-@ kstuges

--- java-core-lessons/src/com/demo/lesson6/homework/Default.java
+++ java-core-lessons/src/com/demo/lesson6/homework/Default.java
@@ -6,4 +6,19 @@ public class Default {
     public String capital;
     public int population;
     public int emigration;
+
+    public Default(String republic, String capital, int population, int emigration) {
+        this.republic = republic;
+        this.capital = capital;
+        this.population = population;
+        this.emigration = emigration;
+
+    }
+
+    public void world() {
+        System.out.println("erkir " + republic);
+        System.out.println("mayraqaxaq " + capital);
+        System.out.println("bnakchutyun " + population);
+        System.out.println("artagaxt@ " + emigration);
+    }
 }
--- java-core-lessons/src/com/demo/lesson6/homework/DefaultDemo.java
+++ java-core-lessons/src/com/demo/lesson6/homework/DefaultDemo.java
@@ -2,16 +2,7 @@ package com.demo.lesson6.homework;
 
 public class DefaultDemo {
     public static void main(String[] args) {
-        Default world = new Default();
-
-        world.republic = " Armenia ";
-        world.capital = " Erevan ";
-        world.population = 2_000_000;
-        world.emigration = 1_000_000;
-
-        System.out.println("erki " + world.republic);
-        System.out.println("mayraqaxaq " + world.capital);
-        System.out.println("bnakchutyun " + world.population);
-        System.out.println("artadaxt " + world.emigration);
+        Default world = new Default("Armenia", "Erevan", 2_000_000, 1_000_000);
+world.world();
     }
 }
