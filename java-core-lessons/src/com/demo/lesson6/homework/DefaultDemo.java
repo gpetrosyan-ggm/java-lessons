d3dd33ab227d5fa7206ebead3ee9d53a5151265a
Arnold <arnoldmartirosyan87@gmail.com>
2022-04-10 01:44:18 +0400

    cheyi dzel toxer@, hima dzeci

--- java-core-lessons/src/com/demo/lesson6/homework/DefaultDemo.java
+++ java-core-lessons/src/com/demo/lesson6/homework/DefaultDemo.java
@@ -3,6 +3,6 @@ package com.demo.lesson6.homework;
 public class DefaultDemo {
     public static void main(String[] args) {
         Default world = new Default("Armenia", "Erevan", 2_000_000, 1_000_000);
-world.world();
+        world.world();
     }
 }
