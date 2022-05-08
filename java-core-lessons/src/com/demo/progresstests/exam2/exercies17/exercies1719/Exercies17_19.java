44d8011ebc13b9d46ab8b2797a6af91792099e56
Arnold <arnoldmartirosyan87@gmail.com>
2022-05-08 18:07:47 +0400

    axper jan, apsos stex smaylik ches karum dnes, 17-19 finish

---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies1719/Exercies17_19.java
@@ -0,0 +1,21 @@
+package com.demo.progresstests.exam2.exercies17.exercies1719;
+/*
+Напишите метод, который получает в качестве параметров два целых числа.
+Метод возвращает значение true, если первая цифра в первом параметре
+равна последней цифре во втором параметре. В ином случае метод возвращает значение false.
+ */
+
+public class Exercies17_19 {
+    void number(int num1, int num2) {
+        boolean ret;
+        while (num1 > 9) {
+            num1 /= 10;
+        }
+        if (num1 == num2 % 10) {
+            ret = true;
+        } else {
+            ret = false;
+        }
+        System.out.println(ret);
+    }
+}
---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies1719/ExerciesDemo17_19.java
@@ -0,0 +1,8 @@
+package com.demo.progresstests.exam2.exercies17.exercies1719;
+
+public class ExerciesDemo17_19 {
+    public static void main(String[] args) {
+        Exercies17_19 exercies17_19 = new Exercies17_19();
+        exercies17_19.number(123, 341);
+    }
+}
