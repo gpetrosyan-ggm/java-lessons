4668f2e83a5e4e18f022f73450c794d7c89a0fda
Arnold <arnoldmartirosyan87@gmail.com>
2022-05-08 15:19:26 +0400

    17.10 verjacreci

---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies1710/Exercies17_10.java
@@ -0,0 +1,16 @@
+package com.demo.progresstests.exam2.exercies17.exercies1710;
+/*
+Напишите метод, который принимает в качестве параметров целое число
+и возвращает сумму его цифр.
+ */
+
+public class Exercies17_10 {
+    public void  amount(int num){
+        int sum =0;
+        while (num>0){
+            sum += num%10;
+            num /=10;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies1710/ExerciesDemo17_10.java
@@ -0,0 +1,8 @@
+package com.demo.progresstests.exam2.exercies17.exercies1710;
+
+public class ExerciesDemo17_10 {
+    public static void main(String[] args) {
+        Exercies17_10 exercies17_10 = new Exercies17_10();
+        exercies17_10.amount(-12);
+    }
+}
