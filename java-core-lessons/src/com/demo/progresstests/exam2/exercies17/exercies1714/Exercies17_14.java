27522174267c6e14ffc03f1bc2a39e93c20a2ba7
Arnold <arnoldmartirosyan87@gmail.com>
2022-05-08 16:44:14 +0400

    17.14 mi tesak shat primitiv er, ajqis mi ban en chem arel

---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies1714/Exercies17_14.java
@@ -0,0 +1,13 @@
+package com.demo.progresstests.exam2.exercies17.exercies1714;
+/*
+Напишите метод, который принимает в качестве параметра два целых
+числа и возвращает среднее арифметическое сумм цифр в этих числах.
+ */
+
+public class Exercies17_14 {
+    void average(int num1, int num2) {
+        System.out.println((num1 + num2) / 2);
+    }
+
+
+}
---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies1714/ExerciesDemo17_14.java
@@ -0,0 +1,11 @@
+package com.demo.progresstests.exam2.exercies17.exercies1714;
+
+
+public class ExerciesDemo17_14 {
+    public static void main(String[] args) {
+        Exercies17_14 exercies17_14 = new Exercies17_14();
+        exercies17_14.average(12,22);
+
+    }
+
+}
