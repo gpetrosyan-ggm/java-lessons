69c2adf94649bedee4d4bb601c9941936bafb85b
Arnold <arnoldmartirosyan87@gmail.com>
2022-05-08 13:24:08 +0400

    Sensel giti vra cunamiya tirum, grohela vor grohum em, 17.5 @nduni

---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies175/Exercies17_5.java
@@ -0,0 +1,38 @@
+package com.demo.progresstests.exam2.exercies17.exercies175;
+/*
+static int secret (int х, int у ) {
+    return (х +у - Math.abs(x - y)) / 2;
+}
+
+1. Какое значение возвращает метод, если его вызов выглядит следующим образом:
+   x=secret(8,12)? Обоснуйте свой ответ.
+
+   // (х +у - Math.abs(x - y)) / 2 -> (8 + 12 - Mat.abs(8-12)) /2; (20 - Math.abs(-4))/2;
+   // Mat.abs(-4) = 4
+   // (20 - 4) / 2 = 8;
+   return 8;
+
+2. Какое значение возвращает метод, если его вызов выглядит следующим образом:
+   x=secret(10,6)? Обоснуйте свой ответ.
+
+   // (х +у - Math.abs(x - y)) / 2 -> (10 + 6 - Math.abs(10-6)) / 2;
+   // (16 - 4) /2 = 12 / 2 = 6;
+   return 6;
+
+3. Какое значение возвращает метод, если его вызов выглядит следующим образом:
+    x=secret(secret(8,12), secret(10,6))? Обоснуйте свой ответ.
+
+    // secret(8,12) -> @st verevi lucaci x argument@ klini 8;
+    // secret(10,6) -> y argument@ klini 6;
+    // x = secret(8,6)
+    // (х +у - Math.abs(x - y)) / 2 -> (8 + 6 - Math.abs(8 - 6)) / 2
+    // (14 - 2) / 2 = 6
+    // return 6;
+
+4. Приведите пример параметров, для которых метод будет возвращать значение О.
+
+   // retur 0 klini miayn mi depqum erb argumentner@ linen 0 seqret(0,0), karam matematikoren lucem u apacucem
+ */
+
+public class Exercies17_5 {
+}
