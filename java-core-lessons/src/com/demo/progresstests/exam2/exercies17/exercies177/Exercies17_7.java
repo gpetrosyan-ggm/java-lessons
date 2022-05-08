3be3cdcb8300c504eaf2e884c85de5a6b04dd0cb
Arnold <arnoldmartirosyan87@gmail.com>
2022-05-08 14:03:48 +0400

    Sensey 17.7-@ arden git-um e

---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies177/Exercies17_7.java
@@ -0,0 +1,52 @@
+package com.demo.progresstests.exam2.exercies17.exercies177;
+/*
+Дан метод:
+
+static int secret(int num) {
+    int temp;
+    if(num < 50){
+     while(num > 2) {
+        num = num - 2;
+        }
+    } else{
+        while(num < 1OO) {
+            num = num + 2;
+        }
+    }
+    if(num == 2 || num = 1OO) {
+        temp = 1;
+    } else {
+        temp = 0;
+    }
+    return temp;
+}
+
+1. Что будет выведено на экран, если вызов метода выглядит следующим образом:
+   System.out.println(secret(1O))? Обоснуйте свой ответ.
+
+   // if(num < 50), bavararum e payman@ kmtni if,  while(num > 2), kmtni cikl u kkatari num -=2;
+   // minjev num = 2, while payman@ chi bavaravi u ciklic durs kga @ndunelov num = 2 arjeq@:
+   // kancni hajord qaylin if(num == 2 || num = 1OO) payman@ kbavararvi u temp-@ k@nduni 1;
+   // return 1
+
+2. Что будет выведено на экран, если вызов метода выглядит следующим образом:
+   System.out.println(secret(5))? Обоснуйте свой ответ.
+
+   // if(num < 50) payman@ bavaraum e,while(num > 2){ num = num - 2}, gorcoxutyun@ texi kunena aynqan
+   // minjev num = 1 arjeq@ @nduni;
+   // kancni hajord qayli if(num == 2 || num = 1OO) payman@ chi bavararvi u kmtni else, temp = 0;
+   // return 0
+
+3. Что будет выведено на экран, если вызов метода выглядит следующим образом:
+   System.out.println(secret(88))? Обоснуйте свой ответ.
+
+   // if(num < 50) payman@ chi bavararum, kmtni else while(num < 1OO) { num = num + 2};
+   // cikl@ kfra minjev num-@ k@nduni 100 arjeq@, durs kga while-ic u kancni hajord qaylin;
+   // if(num == 2 || num = 1OO) kbavarari payman@ u kkatari hamapatasxan logikan, k@nduni temp = 1;
+   // return 1
+
+
+ */
+
+public class Exercies17_7 {
+}
