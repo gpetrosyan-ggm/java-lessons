4d30a42d133605951b0b72baa885007897067ae6
Arnold <arnoldmartirosyan87@gmail.com>
2022-05-08 18:48:23 +0400

    17-22 verjacreci

---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies1722/Exercies17_22.java
@@ -0,0 +1,19 @@
+package com.demo.progresstests.exam2.exercies17.exercies1722;
+/*
+Напишите метод, который принимает в качестве параметра коэффициенты квадратного уравнения
+(а, Ь и с) и возвращает значение true, если уравнение имеет хотя бы одно решение (корень).
+В ином случае метод возвращает значение false.
+ */
+
+public class Exercies17_22 {
+    public void equations(int a, int b, int c) {
+        boolean root;
+        if (a == 0 || (b * b - 4 * a * c) >= 0) {
+            root = true;
+
+        } else {
+            root = false;
+        }
+        System.out.println(root);
+    }
+}
