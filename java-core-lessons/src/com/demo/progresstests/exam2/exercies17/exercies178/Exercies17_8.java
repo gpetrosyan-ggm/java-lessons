049c0ff5ced018dc8ed08af9a8c53deba3315dac
Arnold <arnoldmartirosyan87@gmail.com>
2022-05-08 14:15:54 +0400

    Sensey es temperov vor gnam, irikun@ daj@ das kanenq

---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies178/Exercies17_8.java
@@ -0,0 +1,23 @@
+package com.demo.progresstests.exam2.exercies17.exercies178;
+/*
+Напишите метод, который принимает в качестве параметра число и
+возвращает:
+• -1 - в случае если число отрицательное;
+• - в случае если число равно О;
+• 1 - в случае если число положительное.
+ */
+// Sensey mer es paragayum eyakan chi dataType-@ incha int te double, bayc ete lurj logika greluc lines,
+// int gres te double
+
+public class Exercies17_8 {
+    public void number(double num){
+        if (num < 0){
+            num = -1;
+        }else if (num == 0){
+            num = 0;
+        }else {
+            num = 1;
+        }
+        System.out.println((int) num);
+    }
+}
