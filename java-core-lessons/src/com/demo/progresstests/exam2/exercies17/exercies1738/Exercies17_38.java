287dae09a5452c52efe0ab4e7a7829b43446b8d3
Arnold <arnoldmartirosyan87@gmail.com>
2022-05-09 14:49:05 +0400

    Sensey mnac 2 hat

---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies1738/Exercies17_38.java
@@ -0,0 +1,27 @@
+package com.demo.progresstests.exam2.exercies17.exercies1738;
+/*
+Напишите метод, который получает в качестве параметров два значения - координаты (Х и У) точки А.
+метод возвращает одно из значений 1, 2, 3 или 4 - в зависимости от того,
+в какой четверти системы координат находится точка.
+В случае если точка находится на, как минимум, одной из осей координат, метод возвращает -1.
+ */
+
+public class Exercies17_38 {
+
+    void cordinat(int corX, int corY) {
+        int dot;
+
+        if (corX > 0 && corY > 0) {
+            dot = 1;
+        } else if (corX > 0 && corY < 0) {
+            dot = 2;
+        } else if (corX < 0 && corY < 0) {
+            dot = 3;
+        } else if (corX < 0 && corY > 0) {
+            dot = 4;
+        } else {
+            dot = -1;
+        }
+        System.out.println(dot);
+    }
+}
---
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies17/exercies1738/ExerciesDemo17_38.java
@@ -0,0 +1,8 @@
+package com.demo.progresstests.exam2.exercies17.exercies1738;
+
+public class ExerciesDemo17_38 {
+    public static void main(String[] args) {
+        Exercies17_38 exercies17_38 = new Exercies17_38();
+        exercies17_38.cordinat(0,6);
+    }
+}
