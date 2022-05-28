3be9e96b43a7703a309385c49b81778df652861b
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2022-05-28 15:16:42 +0400

    updated todos

--- java-core-lessons/src/com/demo/progresstests/exam2/exercies19/exercies1966/ExerciesDemo19_66.java
+++ java-core-lessons/src/com/demo/progresstests/exam2/exercies19/exercies1966/ExerciesDemo19_66.java
@@ -5,5 +5,6 @@ public class ExerciesDemo19_66 {
         Exercies19_66 exercies19_66 = new Exercies19_66();
         System.out.println(exercies19_66.massive(new int[]{4, 3, 6, 2, 1, 3, 1}));
         System.out.println(exercies19_66.massive(new int[]{4, 3, 6, 2, -1, -3, -1}));
+        System.out.println(exercies19_66.massive(new int[]{0, 0, 6, 2}));
     }
 }
