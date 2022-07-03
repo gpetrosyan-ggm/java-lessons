0603b412afb38567fb3ebdc4fbeca92630fcb214
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2022-07-03 19:55:54 +0400

    enums more examples

--- java-core-lessons/src/com/demo/lesson12/classroom/enums/Answers.java
+++ java-core-lessons/src/com/demo/lesson12/classroom/enums/Answers.java
@@ -5,14 +5,27 @@ public enum Answers {
     YES(5, "yes"),
     NO(2, "no"),
     MAYBE(3, "maybe"),
-    UNKNOWN(1, "unknown");
+    UNKNOWN(1, "unknown"),
+    UNKNOWN_1(1, "unknown", "AAA"),
+    TEST(1);
 
-    private final int rating;
-    private final String name;
+    private int rating;
+    private String name;
+    private String aa;
 
     Answers(int rating, String name) {
         this.rating = rating;
         this.name = name;
+    }
+
+    Answers(int rating, String name, String a) {
+        this.rating = rating;
+        this.name = name;
+        this.aa = a;
+    }
+
+    Answers(int rating) {
+        this.rating = rating;
     }
 
     public int getRating() {
